a96ff25f34138cdc69c9dc121a78db5e56cbfab1
kstefan <kstefan@kstefans-MacBook-Pro.local>
2021-05-18 15:25:15 +0200

    Add function that will contain smaller functions to start the game, play the match, update the score, etc.

--- mentoring-18-05-21/rock-paper-scissors/app.js
+++ mentoring-18-05-21/rock-paper-scissors/app.js
@@ -1,3 +1,34 @@
 const playGame = () => {
-  // code goes here...
-}
+  // Initial scores for both players
+  let playerScore = 0;
+  let computerScore = 0;
+
+  // Function to start the game
+  const startGame = () => {
+    const playBtn = document.querySelector('.intro button');
+    const introScreen = document.querySelector('.intro');
+    const match = document.querySelector('.match');
+
+    playBtn.addEventListener('click', () => {
+      introScreen.classList.add('fadeOut');
+      match.classList.add('fadeIn');
+    });
+  };
+
+  // Function to play the match
+  const playMatch = () => {
+
+  }
+
+  // Call all the inner functions: startGame, updateScore, etc..
+  startGame();
+  // Function that compares the hands
+
+  // Function that updates the score
+
+
+};
+
+playGame();
+
+
